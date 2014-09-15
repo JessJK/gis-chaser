40f7f7c089828d8e50b70517a54eef75db777070
Ninjah45 <Ben.tehira@gmail.com>
2014-09-16 10:46:19 +1200

    initial project

---
+++ GIS Chaser/GIS Chaser/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GIS_Chaser.Startup))]
+namespace GIS_Chaser
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
