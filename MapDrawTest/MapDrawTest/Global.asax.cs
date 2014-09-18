adb555d3709173e04614c5578090d752f7f14731
Sleepysaurus <ylegaspi@gmail.com>
2014-09-18 15:41:11 +1200

    saved and closed Javascript mapping file

--- MapDrawTest/MapDrawTest/Global.asax.cs
+++ MapDrawTest/MapDrawTest/Global.asax.cs
@@ -12,6 +12,7 @@ namespace MapDrawTest
         protected void Application_Start()
         {
             GlobalConfiguration.Configure(WebApiConfig.Register);
+            GlobalConfiguration.Configure(RouteConfig.RegisterRoutes);
         }
     }
 }
