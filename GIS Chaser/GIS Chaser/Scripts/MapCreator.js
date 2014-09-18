4b5f5e0ffeda0e34005b7c482377bcd7a4454086
Sleepysaurus <ylegaspi@gmail.com>
2014-09-18 17:15:46 +1200

    have centred the hexagon

---
+++ GIS Chaser/GIS Chaser/Scripts/MapCreator.js
@@ -0,0 +1,99 @@
+﻿function initialize(data) {
+    console.log(data);
+    var mapOptions = {
+        center: { lat: data.coords.latitude, lng: data.coords.longitude },
+        zoom: 18
+    };
+
+    console.log(mapOptions);
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+
+    
+    var hex = new Hex(data.coords.latitude, data.coords.longitude, 0.001);
+
+    var properHex = hex.GetHexPoly();
+
+    console.table(properHex);
+    var myCoordinates = [];
+
+    $.each(properHex, function (index, value) {
+        myCoordinates.push(new google.maps.LatLng(value.lat, value.lng));
+    });
+
+    var polyOptions = {
+        path: myCoordinates,
+        strokeColor: "#FF0000",
+        strokeOpacity: 1,
+        strokeWeight: 1
+    }
+
+    var hex = new google.maps.Polygon(polyOptions);
+    hex.setMap(map);
+
+   
+
+}
+
+function Hex(latitude, longitude, size) {
+
+    this.centreRadiusLatitude = latitude;
+    this.centreRadiusLongitude = longitude;
+
+     this.offsetLatitude = latitude;
+     this.offsetLongitude = longitude;
+     this.width = (size * 2) * (1 / Math.sin(latitude) * 0.8);
+     this.height = Math.sqrt(3) / 2.4 * this.width;
+    
+
+
+    //given hex get out a hexpoly array which is hex maker
+
+}
+
+Hex.prototype.GetHexPoly = function() {
+    
+    //from here we are calling functions hex, hexfromradiuspoint, cornerfromcentre, hexmaker
+    this.CornerFromCentre();
+    return this.HexMaker();
+
+
+}
+Hex.prototype.HexMaker = function() {
+    var hexArray = [
+        { lat: this.offsetLatitude,                    lng: this.offsetLongitude },                   //1
+        { lat: this.offsetLatitude,                    lng: this.offsetLongitude + (1 / 2 * this.width) }, //2
+        { lat: this.offsetLatitude - (1 / 2 * this.height), lng: this.offsetLongitude + (3 / 4 * this.width) }, //3
+        { lat: this.offsetLatitude - this.height,           lng: this.offsetLongitude + (1 / 2 * this.width) }, //4
+        { lat: this.offsetLatitude - this.height,           lng: this.offsetLongitude + 0 },               //5
+        { lat: this.offsetLatitude - (1 / 2 * this.height), lng: this.offsetLongitude - (1 / 4 * this.width) }, //6
+        { lat: this.offsetLatitude,                    lng: this.offsetLongitude }                    //7
+    ];
+
+
+
+    //$.post("/api/hexcoords", JSON.stringifyhexArray));
+    return hexArray;
+}
+
+
+Hex.prototype.CornerFromCentre = function() {
+
+    //find corner point, half height and -1/4 width.
+    this.offsetLatitude = this.centreRadiusLatitude + (1/2 * this.height);
+    this.offsetLongitude = this.centreRadiusLongitude - (1/4 * this.width);
+
+    
+
+}
+
+
+
+function setUpLocation() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(initialize);
+    }
+}
+google.maps.event.addDomListener(window, 'load', setUpLocation);
+console.log("Info should be coming through");
